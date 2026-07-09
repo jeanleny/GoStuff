e67dd98d18cd1a08fdeb35ea59dccb5a044a8c4e
lenyperis@hotmail.fr <lenyperis@hotmail.fr>
2026-07-09 19:55:51 +0200

    master [ADD] add the update erning loop

--- clicker/internal/items/items.go
+++ clicker/internal/items/items.go
@@ -1,28 +1,31 @@
 package items
 
 type Object interface{
-	GetPrice()	int
-	GetEarn()   int
+	GetPrice()	float64	
+	GetEarn()   float64
 	GetAmount() int
 	AddAmount(int) 
 	Buy()
 }
 
 type ObjStats struct {
-	Price int
-	Earn int
-	Amount int
+	Price	float64
+	Earn 	float64
+	Amount	int
 }
 
 type Worker struct {ObjStats}
 type Factory struct {ObjStats}
 type Company struct {ObjStats}
 
-func (obj *ObjStats) GetEarn() int {return obj.Earn}
-func (obj *ObjStats) GetPrice() int {return obj.Price}
+func (obj *ObjStats) GetEarn() float64 {return obj.Earn}
+func (obj *ObjStats) GetPrice() float64 {return obj.Price}
 func (obj *ObjStats) GetAmount() int {return obj.Amount}
 
 func (b *ObjStats) Buy() {} //le truc cheloouuuuuu type func buy() = 0 en cPP
+func (obj *Business) Work() {
+	obj.Money++
+}
 
 func (obj *Business) Buy (name string) bool {
 	choice := obj.Stock[name]
@@ -32,12 +35,14 @@ func (obj *Business) Buy (name string) bool {
 	}
 	obj.Money -= price
 	choice.AddAmount(1);
+	obj.Earning += choice.GetEarn()
 	return (true)
 }
 
 type Business struct {
 	Stock map[string]Object
-	Money int
+	Money float64
+	Earning float64
 }
 
 func (obj *ObjStats) AddAmount(n int){
